d2e3ae8dc17a57ba1ff1cff18866edeed1f3a88f
LiisM2tlik <liis.matlik@gmail.com>
2024-05-17 14:33:30 +0300

    Password 2 times

--- register.php
+++ register.php
@@ -14,11 +14,22 @@
                     <h3>Registreeru</h3>
                     <div class="form-group">
                         <label for="username">Kasutajanimi</label>
-                        <input name="username" type="number" class="form-control" id="username">
+                        <input name="username" type="text" class="form-control" id="username">
                     </div>
                     <div class="form-group">
-                        <label for="password">Parool</label>
-                        <input name="password" type="text" class="form-control" id="username">
+                        <label for="password1">Parool</label>
+                        <input name="password" type="password" class="form-control" id="password1">
+                    </div>
+                    <div class="form-group">
+                        <label for="password2">Parool</label>
+                        <input name="password" type="password" class="form-control" id="password2">
+                        <?php 
+                        if (isset($_POST['password']) && isset($_POST['password2'])) {
+                            if ($_POST['password'] != $_POST['password2']) { 
+                                print"Paroolid ei kattu";
+                            }
+                        }
+                        ?>
                     </div>
                     <button type="submit" class="btn btn-dark mt-3 w-100">Registreeru</button>
                 </div>
