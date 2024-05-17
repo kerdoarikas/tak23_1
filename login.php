5dede6e75eee06cc0b5aea3a5bcebdb0c156e435
LiisM2tlik <liis.matlik@gmail.com>
2024-05-17 13:41:13 +0300

    Login types changed

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
                     </div>
                     <div class="form-group">
                         <label for="password">Parool</label>
-                        <input name="password" type="text" class="form-control" id="username">
+                        <input name="password" type="password" class="form-control" id="password">
                     </div>
                     <button type="submit" class="btn btn-dark mt-3 w-100">Sisene</button>
                 </div>
